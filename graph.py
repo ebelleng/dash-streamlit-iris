3769c27365bf8aaf3f8f72cb1baaf26c94ac4009
ebelleng <etienne.bellenger@gmail.com>
2023-06-05 14:25:06 -0400

    test

--- graph.py
+++ graph.py
@@ -19,44 +19,48 @@ y = iris.target
 x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
 y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
 
-plt.figure(2, figsize=(8, 6))
-plt.clf()
 
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
-plt.xlabel("Sepal length")
-plt.ylabel("Sepal width")
 
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
+def get_gp(x_min=x_min,y_min=y_min,X=X,y=y):
+    plt.figure(2, figsize=(8, 6))
+    plt.clf()
 
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
-fig = plt.figure(1, figsize=(8, 6))
-ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
+    # Plot the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+    plt.xlabel("Sepal length")
+    plt.ylabel("Sepal width")
 
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(
-    X_reduced[:, 0],
-    X_reduced[:, 1],
-    X_reduced[:, 2],
-    c=y,
-    cmap=plt.cm.Set1,
-    edgecolor="k",
-    s=40,
-)
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
 
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.zaxis.set_ticklabels([])
+    # To getter a better understanding of interaction of the dimensions
+    # plot the first three PCA dimensions
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
 
-plt.show()
+    X_reduced = PCA(n_components=3).fit_transform(iris.data)
+    ax.scatter(
+        X_reduced[:, 0],
+        X_reduced[:, 1],
+        X_reduced[:, 2],
+        c=y,
+        cmap=plt.cm.Set1,
+        edgecolor="k",
+        s=40,
+    )
+
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.zaxis.set_ticklabels([])
+
+    plt.show()
+    return plt
 
 def get_df():
     df = pd.DataFrame({
